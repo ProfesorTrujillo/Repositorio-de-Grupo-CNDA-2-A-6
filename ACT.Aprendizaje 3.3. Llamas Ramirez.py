2438bf9432ec3fa42ca5c40d8ab36397e1a4ce6f
Angel Andany Llamas Ramirez <andanywork44@gmail.com>
2026-03-26 12:56:35 -0600

    Add files via upload

---
+++ ACT.Aprendizaje 3.3. Llamas Ramirez.py
@@ -0,0 +1,11 @@
+# Pedimos los nombres y los guardamos directamente
+juego1 = input("Dime el primer videojuego: ")
+juego2 = input("Dime el segundo videojuego: ")
+juego3 = input("Dime el tercer videojuego: ")
+
+#  (lista)
+lista_juegos = [juego1, juego2, juego3]
+
+# Mostramos el resultado
+print("Aquí tienes tu lista:")
+print(lista_juegos)
