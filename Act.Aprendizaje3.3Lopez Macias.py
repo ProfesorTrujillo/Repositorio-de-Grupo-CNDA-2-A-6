8a636b4d753bfb9ed1826fdc297d63e65eaeb120
Robberto-10 <250950@utags.edu.mx>
2026-03-26 13:00:46 -0600

    Create Act.Aprendizaje3.3Lopez Macias.py

---
+++ Act.Aprendizaje3.3Lopez Macias.py
@@ -0,0 +1,10 @@
+videojuegos=[]
+
+for i in range(3):
+    juego = input("Escribe un videojuego: ")
+    videojuegos.append(juego)
+
+print("Los videojuegos son:")
+
+for i in range(3):
+    print(videojuegos[i])
