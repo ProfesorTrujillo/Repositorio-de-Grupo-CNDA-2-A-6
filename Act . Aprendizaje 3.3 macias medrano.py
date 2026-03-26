4f0cd9404574a48744f0d1ac6683886357cc6caf
ammm0731 <250574@utags.edu.mx>
2026-03-26 12:57:34 -0600

    Add files via upload

---
+++ Act . Aprendizaje 3.3 macias medrano.py
@@ -0,0 +1,14 @@
+juegos = []
+
+j1 = input("Dame un videojuego: ")
+juegos.append(j1)
+
+j2 = input("Dame otro videojuego: ")
+juegos.append(j2)
+
+j3 = input("Dame otro más: ")
+juegos.append(j3)
+
+# mostrar el arreglo
+print("Tus videojuegos son:")
+print(juegos)
