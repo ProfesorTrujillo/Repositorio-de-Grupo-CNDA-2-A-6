1bb4ea4a3d5ab050eda44fa26aa0eae468e6acb4
ammm0731 <250574@utags.edu.mx>
2026-03-26 13:41:10 -0600

    Update Act . Aprendizaje_3.3_MaciasMedrano.py
    Entrega de mi programa para la Actividad 3.3

--- Act . Aprendizaje_3.3_MaciasMedrano.py
+++ Act . Aprendizaje_3.3_MaciasMedrano.py
@@ -1,14 +1,49 @@
 juegos = []
 
-j1 = input("Dame un videojuego: ")
-juegos.append(j1)
+videojuegos = []
 
-j2 = input("Dame otro videojuego: ")
-juegos.append(j2)
+opcion = 0
 
-j3 = input("Dame otro más: ")
-juegos.append(j3)
+while opcion != 5:
 
-# mostrar el arreglo
-print("Tus videojuegos son:")
-print(juegos)
+    print("----- MENU -----")
+    print("1. Agregar videojuego")
+    print("2. Mostrar lista completa")
+    print("3. Buscar videojuego")
+    print("4. Eliminar videojuego")
+    print("5. Salir")
+
+    opcion = int(input("Elige una opcion: "))
+
+    if opcion < 1 or opcion > 5:
+        print("Opcion invalida")
+
+    elif opcion == 1:
+        nombre = input("Escribe el nombre del videojuego: ")
+        videojuegos.append(nombre)
+        print("se agrego nuevo videojuego")
+
+    elif opcion == 2:
+        if videojuegos == []:
+            print("Aun no se registran videojuegos")
+        else:
+            for x in videojuegos:
+                print(x)
+
+    elif opcion == 3:
+        nombre = input("Que videojuego quieres encontrar: ")
+        if nombre in videojuegos:
+            print("Videojuego encontrado")
+        else:
+            print("No esta en la lista loco")
+
+    elif opcion == 4:
+        nombre = input("Escribe el videojuego a eliminar: ")
+        if nombre in videojuegos:
+            videojuegos.remove(nombre)
+            print("Videojuego eliminado")
+        else:
+            print("No esta en la lista loco")
+
+    elif opcion == 5:
+        print("Programa finalizado")
