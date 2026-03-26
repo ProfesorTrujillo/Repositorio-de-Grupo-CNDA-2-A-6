7e7711f02280f9dd7e46e803b4bd6242cebc0dc8
Sergio-Cruz-23 <ivanibarracruz246@gmail.com>
2026-03-26 12:57:53 -0600

    Add files via upload
    Actividad de leer 3 videojuegos

---
+++ Act. Aprendizaje 3.3 Cruz Ibarra.py
@@ -0,0 +1,6 @@
+videojuego1=input("Dame nombre del primer videojuego: ")
+videojuego2=input("Dame nombre del segundo videojuego: ")
+videojuego3=input("Dame nombre del tercer videojuego: ")
+
+videojuego=[videojuego1 , videojuego2 , videojuego3 ]
+print(videojuego)
