d5f7eee3e1f3fdc2d362fcd4844789b42459e7a1
Connie3101 <250980@utags.edu.mx>
2026-03-26 13:18:38 -0600

    Add files via upload

---
+++ act_aprendizaje_3.3_Marquez Garcia.py
@@ -0,0 +1,10 @@
+#crear una lista de 3 videojuegos haciendo uso de los arreglos
+juegos = []
+#pedirle los nombres de los videojuegos
+juegos.append(input("nombre del videojuego"))
+juegos.append(input("nombre del videojuego"))
+juegos.append(input("nombre del videojuego"))
+#mostrar la lista completa
+print ("la lista de videojuegos es:" )
+print(juegos)
+
