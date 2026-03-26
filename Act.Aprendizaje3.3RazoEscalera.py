bfb8059945fdd3035a59831da8fba5286473776c
paolarazo <250911@utags.edu.mx>
2026-03-26 13:49:37 -0600

    Update Act.Aprendizaje3.3RazoEscalera.py
    Entrega de mi programa para la Actividad 3.3 listo para revisar

--- Act.Aprendizaje3.3RazoEscalera.py
+++ Act.Aprendizaje3.3RazoEscalera.py
@@ -1,11 +1,47 @@
-#crear el arreglo
 videojuegos = []
-#pedir al usuario que ingrese tres nombres de videojeugos 
-print("ingresa el nombre de 3 videojuegos:")
-#como solo son 3 juegos hacemos uso de la estructura for en el que repita la pregunta tres veces y lo guardamos en el arreglo
-for i in range(3):
-    nombredejuego = input("Videojuego " + str(i + 1) + ": ")
-    videojuegos.append(nombredejuego)
-#mandamos a llamar al arreglo
-print("Los videojuegos son :")
-print(videojuegos)
+seleccion = 0
+
+def menu():
+    print("\n1. Agregar videojuego")
+    print("2. Mostrar lista completa")
+    print("3. Buscar videojuego")
+    print("4. Eliminar videojuego")
+    print("5. Salir")
+
+while seleccion != 5:
+    menu()
+    seleccion = int(input("Elige una opción (1-5): "))
+
+    if seleccion == 1:
+        agregado = input("Ingresa el nombre del videojuego: ")
+        videojuegos.append(agregado)
+        print("Videojuego agregado")
+        
+    elif seleccion == 2:
+        if len(videojuegos) == 0:
+            print("No hay videojuegos registrados.")
+        else:
+            print("Lista de juegos:")
+            for juego in videojuegos:
+                print(juego)
+                
+    elif seleccion == 3:
+        nombre_buscar = input("Nombre del videojuego a buscar: ")
+        if nombre_buscar in videojuegos:
+            print("Videojuego encontrado.")
+        else:
+            print("No se encuentra en la lista.")
+            
+    elif seleccion == 4:
+        nombre_eliminar = input("Nombre del videojuego a eliminar: ")
+        if nombre_eliminar in videojuegos:
+            videojuegos.remove(nombre_eliminar)
+            print("Videojuego eliminado.")
+        else:
+            print("No está en la lista.")
+            
+    elif seleccion == 5:
+        print("Programa finalizado.")
+        
+    else:
+        print("Opción inválida.")
