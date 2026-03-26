8471697577cde3bf1f9be647e6e3e7eae4aefa44
R_R_458 <estradareyesroberto242@gmail.com>
2026-03-26 12:57:30 -0600

    Add files via upload

---
+++ Act. Aprendizaje 3.3 Estrada Reyes.py
@@ -0,0 +1,10 @@
+# Crear lista vacía
+videojuegos = []
+
+# Pedir 3 nombres
+for i in range(3):
+    nombre = input("Escribe un videojuego: ")
+    videojuegos.append(nombre)
+
+# Mostrar la lista
+print("Los videojuegos son:", videojuegos)
