ee6d8867abe4fe234978a695acc6646957537501
Sergio-Cruz-23 <ivanibarracruz246@gmail.com>
2026-03-26 13:43:06 -0600

    Entrega de mi programa para la Actividad 3.3

--- Act. Aprendizaje 3.3 Cruz Ibarra.py
+++ Act. Aprendizaje 3.3 Cruz Ibarra.py
@@ -1,6 +1,40 @@
-videojuegos=[]
+opcion = 0
+videojuegos = []
 
-for i in range(3):
-    videojuego=input("Ingresa tu Videojuego: ")
-    videojuegos.append(videojuego)
-print("Tus juegos son", videojuegos )
+while opcion != 5:
+    print("\n----- MENÚ -----")
+    print("1. Agregar videojuego")
+    print("2. Mostrar lista completa")
+    print("3. Buscar videojuego")
+    print("4. Eliminar videojuego")
+    print("5. Salir")
+    opcion = int(input("Elige una opción: "))
+    
+if opcion == 1:
+        nombre=input("Dame el nombre del videojuego: ")
+        videojuegos.append(nombre)
+        print("Agregado")
+        
+elif opcion == 2:
+    if videojuegos == []:
+      print("lista vacia")
+    else:
+        for x in videojuegos:
+            print(x)
+elif opcion == 3:
+    buscar=input("Buscar: ")
+    if buscar in videojuegos:
+        print("Si esta")
+    else:
+        print("No esta")
+        
+elif opcion == 4:
+        eliminar = input("Eliminar: ")
+        if eliminar in videojuegos:
+            videojuegos.remove(eliminar)
+            print("Eliminado")
+        else:
+            print("No está")
+elif opcion == 5:
+        print("Fin")
+        
