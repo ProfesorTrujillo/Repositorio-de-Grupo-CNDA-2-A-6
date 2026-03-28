51104bd379b5e2f59f73f0f71b2ead4105d19ba8
Alejandra-Trejo <alejandratrejo1303@gmail.com>
2026-03-27 20:04:15 -0600

    Delete Act.aprendizaje 3.3 TREJO LARA.py

--- Act.aprendizaje 3.3 TREJO LARA.py
+++
@@ -1,8 +0,0 @@
-#Trabajando con Arreglos
-cantidad = int(input("¿Cuántos videojuegos deseas ingresar? "))
-juegos = []
-for i in range(cantidad):
-     juego = input("ingresa el nombre del videojuego {}: ".format(i+1))
-     juegos.append(juego)
-print("Los videojuegos ingresados son:", juegos)
- 
